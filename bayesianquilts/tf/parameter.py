19a291f1bbc0307a71c7aecafc95a39ad9f5d57c
Josh Chang <joshchang@ucla.edu>
2021-12-26 11:39:51 -0500

    inflate and deflate the multi-indices

--- bayesianquilts/tf/parameter.py
+++ bayesianquilts/tf/parameter.py
@@ -21,15 +21,10 @@ def tf_ravel_multi_index(multi_index, dims):
 class Interactions(object):
     _interaction_list = []
 
-    def __init__(
-            self,
-            dimensions,
-            exclusions=None) -> None:
+    def __init__(self, dimensions, exclusions=None) -> None:
         super().__init__()
         self._dimensions = dimensions
-        self._intrinsic_shape = [
-            x[1] for x in self._dimensions
-        ]
+        self._intrinsic_shape = [x[1] for x in self._dimensions]
         self._exclusions = [set(s) for s in exclusions]
 
     def shape(self):
@@ -49,14 +44,15 @@ class Decomposed(object):
     _intrinsic_shape = None
 
     def __init__(
-            self,
-            interactions,
-            param_shape=[],
-            default_val=None,
-            dtype=tf.float32,
-            name="",
-            *args,
-            **kwargs) -> None:
+        self,
+        interactions,
+        param_shape=[],
+        default_val=None,
+        dtype=tf.float32,
+        name="",
+        *args,
+        **kwargs,
+    ) -> None:
 
         super().__init__()
         assert isinstance(
@@ -71,18 +67,18 @@ class Decomposed(object):
         self._name = name
         self._param_shape = param_shape
         self._param_tensors, self._param_interactions = self.generate_tensors()
-        self._param_shapes = {
-            k: v.shape for k, v in self._param_tensors.items()}
+        self._param_shapes = {k: v.shape for k, v in self._param_tensors.items()}
 
-    def generate_tensors(self, batch_shape=None, target=None):
+    def generate_tensors(self, batch_shape=None, target=None, flatten_indices=False):
         """Generate parameter tensors for the parameter decomposition,
            neatly handling TF's limitation in the maximum number of
            axes (6) for a tensor to be used in most common mathematical operations
            like broadcasting.
 
         Args:
-            batch_shape ([type], optional): [description]. Defaults to None.
-            target ([type], optional): [description]. Defaults to None.
+            batch_shape ([list], optional): [description]. Defaults to None.
+            target ([list], optional): [description]. Value to target for decomposition.
+            flatten_indices (boolean): Whether to flatten index dimensions
 
         Returns:
             [type]: [description]
@@ -94,19 +90,23 @@ class Decomposed(object):
         if target is None:
             residual = tf.zeros(batch_shape + self.shape(), self._dtype)
         else:
-            residual = (
-                tf.cast(target, self._dtype) +
-                tf.zeros(batch_shape + self.shape(), self._dtype))
+            residual = tf.cast(target, self._dtype) + tf.zeros(
+                batch_shape + self.shape(), self._dtype
+            )
         for n_tuple in product([0, 1], repeat=self._interactions.rank()):
             interaction = tuple(
-                [t for j, t in enumerate(
-                    self._interactions._dimensions) if n_tuple[j] == 1])
+                [
+                    t
+                    for j, t in enumerate(self._interactions._dimensions)
+                    if n_tuple[j] == 1
+                ]
+            )
             interaction_vars = tuple([x[0] for x in interaction])
             if set(interaction_vars) in self._interactions._exclusions:
                 continue
             interaction_name = "_".join(interaction_vars)
             interaction_shape = (
-                self._interactions.shape()**np.array(n_tuple)
+                self._interactions.shape() ** np.array(n_tuple)
             ).tolist()
 
             tensor_shape = interaction_shape
@@ -115,15 +115,18 @@ class Decomposed(object):
             tensor_shape += self._param_shape
 
             # Set the tensor value
-            tensor_names[self._name +
-                         "__" + interaction_name] = interaction_vars
+            tensor_names[self._name + "__" + interaction_name] = interaction_vars
             value = residual
             for ax, flag in enumerate(n_tuple):
                 if flag == 0:
-                    value = tf.reduce_mean(value, axis=(
-                        batch_ndims+ax), keepdims=True)
+                    value = tf.reduce_mean(
+                        value, axis=(batch_ndims + ax), keepdims=True
+                    )
             tensors[self._name + "__" + interaction_name] = value
-            residual = tf.add_n(broadcast_tensors([residual, -1.0*value]))
+            residual = tf.add_n(broadcast_tensors([residual, -1.0 * value]))
+
+        if flatten_indices:
+            tensors = self.flatten_indices(tensors)
 
         return tensors, tensor_names
 
@@ -131,13 +134,38 @@ class Decomposed(object):
         for k in self._param_tensors.keys():
             self._param_tensors[k] = tensors[k]
 
+    def flatten_indices(self, tensors=None):
+        param_rank = len(self._param_shape)
+        interaction_rank = len(self._interaction_shape)
+        for k in tensors.keys():
+            rank = tf.rank(tensors[k])
+            batch_shape = tensors[k].shape.as_list()[
+                : (rank - param_rank - interaction_rank)
+            ]
+            interaction_shape = self._param_shapes[k][:(-param_rank)]
+            tensors[k] = tf.reshape(
+                tensors[k],
+                batch_shape + [np.prod(interaction_shape)] + self._param_shape,
+            )
+
+        return tensors
+
+    def inflate_indices(self, tensors):
+        param_rank = len(self._param_shape)
+        interaction_rank = len(self._interaction_shape)
+        for k in tensors.keys():
+            rank = tf.rank(tensors[k])
+            batch_shape = tensors[k].shape.as_list()[
+                : (rank - param_rank - 1)
+            ]
+            tensors[k] = tf.reshape(
+                tensors[k], batch_shape + self._param_shapes[k]
+            )
+        return tensors
+
     def __add__(self, x):
         if tf.is_tensor(x):
-            return tf.add_n(
-                broadcast_tensors(
-                    [x, self.constitute()]
-                )
-            )
+            return tf.add_n(broadcast_tensors([x, self.constitute()]))
         return self.constitute().__add__(x)
 
     def __radd__(self, x):
@@ -151,13 +179,14 @@ class Decomposed(object):
 
     def constitute(self, tensors=None):
         tensors = self._param_tensors if tensors is None else tensors
-        partial_sum = tf.zeros(
-            self.shape(),
-            self._dtype)
+        partial_sum = tf.zeros(self.shape(), self._dtype)
+        #  batch
+
+        # folded
         for t in tf.nest.flatten(self._param_tensors):
-            partial_sum = tf.add_n(broadcast_tensors(
-                [partial_sum, tf.cast(t, self._dtype)]
-                ))
+            partial_sum = tf.add_n(
+                broadcast_tensors([partial_sum, tf.cast(t, self._dtype)])
+            )
         return partial_sum
 
     def tensor_keys(self):
@@ -170,29 +199,22 @@ class Decomposed(object):
         # stick batch axes on the  end
         rank = tf.rank(_global)
         permutation = list(range(batch_ndims, rank)) + list(range(batch_ndims))
-        _global = tf.transpose(
-            _global,
-            permutation
-        )
+        _global = tf.transpose(_global, permutation)
         _global = tf.reshape(
             _global,
-            [
-                np.prod(_global.shape[:-(len(self._param_shape)+batch_ndims)])
-            ] + _global.shape[-(len(self._param_shape)+batch_ndims):])
-        interaction_indices = tf.transpose(
-            tf.convert_to_tensor(interaction_indices))
-        indices = tf_ravel_multi_index(
-            interaction_indices, self._interaction_shape)
+            [np.prod(_global.shape[: -(len(self._param_shape) + batch_ndims)])]
+            + _global.shape[(-(len(self._param_shape) + batch_ndims)) :],
+        )
+        interaction_indices = tf.transpose(tf.convert_to_tensor(interaction_indices))
+        indices = tf_ravel_multi_index(interaction_indices, self._interaction_shape)
         _global = tf.gather_nd(_global, indices[:, tf.newaxis])
 
         # move parameter batch dims back to the front
         rank = tf.rank(_global)
-        permutation = list(range(rank-batch_ndims, rank)) + \
-            list(range(rank-batch_ndims))
-        _global = tf.transpose(
-            _global,
-            permutation
+        permutation = list(range(rank - batch_ndims, rank)) + list(
+            range(rank - batch_ndims)
         )
+        _global = tf.transpose(_global, permutation)
 
         return _global
 
@@ -203,22 +225,26 @@ class Decomposed(object):
 def main():
     interact = Interactions(
         [
-            ("planned", 2), ("pre2011", 2),
-            ("mdc", 26), *[(f"hx_{j}", 2) for j in range(7)]],
+            ("planned", 2),
+            ("pre2011", 2),
+            ("mdc", 26),
+            *[(f"hx_{j}", 2) for j in range(5)],
+        ],
         # exclusions=[("Dx",),(),("Dx","Tx","Hx")],
-        exclusions=[*[(f"hx_{j}", ) for j in range(7)], ()]
+        exclusions=[*[(f"hx_{j}",) for j in range(5)], ()],
     )
-    p = Decomposed(interactions=interact,
-                   param_shape=[100, 5], name="beta")
+    p = Decomposed(interactions=interact, param_shape=[20, 2], name="beta")
     indices = [
-        [0, 0, 21, 1, 1, 1, 1, 0, 1, 0],
-        [0, 0, 12, 1, 1, 1, 1, 0, 1, 1],
-        [0, 0, 0, 1, 0, 1, 1, 0, 1, 1],
-        [0, 0, 13, 1, 0, 1, 1, 0, 1, 0],
-        [0, 0, 13, 0, 0, 1, 1, 0, 1, 1]]
+        [0, 0, 21, 1, 1, 1, 1, 0],
+        [0, 0, 12, 1, 1, 1, 1, 1],
+        [0, 0, 0, 1, 0, 1, 1, 1],
+        [0, 0, 13, 1, 0, 1, 1, 0],
+        [0, 0, 13, 0, 0, 1, 1, 1],
+    ]
 
-    t, n = p.generate_tensors(batch_shape=[4])
-    p.set_params(t)
+    t, n = p.generate_tensors(batch_shape=[4], flatten_indices=True)
+    t1 = p.inflate_indices(t)
+    p.set_params(t1)
     r = p.query(indices, t)
     pass
 
