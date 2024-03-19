accb6d8fc27fada22eeb62e5fa894e3a656e517b
joshchangNIH <141774921+joshchangNIH@users.noreply.github.com>
2024-03-19 10:15:43 -0400

    bump

--- setup.py
+++ setup.py
@@ -42,9 +42,8 @@ setuptools.setup(
         # We need to check for direct depends or we can delete scipy.
         'scipy',
         'scikit_learn==1.3.2',
-        'protobuf~=3.19.0',
-        'tensorflow==2.13.0',
-        'tensorflow-probability==0.23.0',
+        'tensorflow==2.16.1',
+        'tensorflow-probability==0.24.0',
         'jax',
         'natsort',
         'tqdm',
