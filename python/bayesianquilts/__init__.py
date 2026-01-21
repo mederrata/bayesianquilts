24d4d423fb64c832acbab79fb4574a0f2d2112fc
jcalifornia <josh@mederrata.com>
2026-01-20 22:38:54 -0500

    Bump version to 0.2.0

--- python/bayesianquilts/__init__.py
+++ python/bayesianquilts/__init__.py
@@ -2,3 +2,5 @@ from .model import BayesianModel
 # from .predictors.classification.logistic_bayesianquilt import \
 #     LogisticBayesianquilt
 from .imputation import MICELogistic
+
+__version__ = "0.2.0"
